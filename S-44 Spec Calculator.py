66b07d5107bfdafce08f0b0159984468f5ef2f00
Dan Cresswell <78384814+DanCressUK@users.noreply.github.com>
2024-04-05 15:37:12 +0100

    Add files via upload
    Finished, but untested properly with calculations. Included test screenshots of my results - they seem sensible enough.

--- S-44 Spec Calculator.py
+++ S-44 Spec Calculator.py
@@ -1,26 +1,7 @@
-#Dictonary:
+from dictionary_module import EO, SO, OA, OB, OT
 
-EO = {
-      "AD": "Areas where there is strict minimum underkeel clearance and manoeuvrability criteria"
-      }
-
-SO = {
-      "AD": "Areas where underkeel clearance is critical."
-      }
-
-OA = {
-      "AD": "Areas where underkeel clearance is considered not to be critical but features of concern to surface shipping may exist."
-      }
-
-OB = {
-      "AD": "Areas where underkeel clearance is not considered to be an issue for the type of surface shipping expected to transit the area."
-      }
-
-OT = {
-      "AD": "Areas where a general description of the seafloor is considered adequate."
-      }
-
-depth = 40
+mindepth = float(input("Input minimum site depth (m): "))
+maxdepth = float(input("Input maxmimum site depth (m): "))
 
 class Order:
     def __init__ (self, a, b, perdep, Area_Description, Depth_THU, Depth_TVU, Feature_Detection, Feature_Search, Bathymetric_Coverage):
@@ -35,48 +16,57 @@ class Order:
         self.Bathymetric_Coverage = Bathymetric_Coverage
     
     def AreaDescription(self):
+        print(" \n=== Criteria calculated below ===\n")
         print(f"{self.Area_Description}")
     
     def DepthTHU(self):
         if {self.perdep} != 1:
-            THU = self.Depth_THU + (self.Depth_THU * (self.perdep - 1))
-            print("Total Horizontal Uncertainty is:", THU, " m")
+            THU_min = round(self.Depth_THU + (mindepth * (self.perdep - 1)), 4)
+            THU_max = round(self.Depth_THU + (maxdepth * (self.perdep - 1)), 4)
+            print("Total Horizontal Uncertainty maximum @ ", mindepth, "m depth is:", THU_min, "m")
+            print("Total Horizontal Uncertainty maximum @ ", maxdepth, "m depth is:", THU_max, "m")
         else:
-            print(f"Total Horizontal Uncertainity is: {self.Depth_THU} m")
+            print(f"Total Horizontal Uncertainity maximum is: {self.Depth_THU} m")
             
     def DepthTVU(self):
-        print(f"{self.Depth_TVU}")
-        
+        TVU_min = round(((self.a)**2+(self.b * mindepth)**2)**0.5, 4)
+        TVU_max = round(((self.a)**2+(self.b * maxdepth)**2)**0.5, 4)     
+        print("Total Vertical Uncertainity maximum @ ", mindepth, "m depth is: ", TVU_min, "m")
+        print("Total Vertical Uncertainity maximum @ ", maxdepth, "m depth is: ", TVU_max, "m")
+        30
     def FeatureDetection(self):
-        print(f"{self.Feature_Detection}")
+        print(f"Feature Detection criteria is: {self.Feature_Detection}")
+        if classinput == "1A":
+            print("O1A: 10% of ", mindepth, "m depth (minimum) is: ", round(mindepth * 0.10, 3), "m")
+            print("O1A: 10% of ", maxdepth, " m depth (maximum) is: ", round(maxdepth * 0.10, 3), "m")
         
     def FeatureSearch(self):
-        print(f"{self.Feature_Search}")
+        print(f"Feature Search criteria is: {self.Feature_Search}")
         
     def BathymetricCoverage(self):
-        print(f"{self.Bathymetric_Coverage}")
+        print(f"Bathymetric Coverage criteria is: {self.Bathymetric_Coverage}")
         
-classinput = input("Select Order:")
+classinput = input("Select Order (Accepts EO, SO, 1A, 1B, 2):")
 
-#a, b, perdep
+#a, b, perdep. AreaDesc, Depth_THU(Percentage), Depth_TVU, Feature_Detec, Feature_Search, Bathymetric Coverage
 
-if classinput == "EO":
-    order = Order(0.15, 0.0075, 1, EO["AD"], 1, 6, 7, 8, 9)
+if classinput == "EO": #AD, FD, FS, BC
+    order = Order(0.15, 0.0075, 1, EO["AD"], 1, 0, EO["FD"], EO["FS"], EO["BC"])
     order.AreaDescription(), order.DepthTHU(), order.DepthTVU(), order.FeatureDetection(), order.FeatureSearch(), order.BathymetricCoverage()
 elif classinput == "SO":
-    order = Order(0.25, 0.0075, 1, SO["AD"], 2, 6, 7, 8, 9)
-    order.AreaDescription(), order.Depth_THU(), order.Depth_TVU(), order.Feature_Detection(), order.Feature_Search(), order.Bathymetric_Coverage()
+    order = Order(0.25, 0.0075, 1, SO["AD"], 2, 0, SO["FD"], SO["FS"], SO["BC"])
+    order.AreaDescription(), order.DepthTHU(), order.DepthTVU(), order.FeatureDetection(), order.FeatureSearch(), order.BathymetricCoverage()
 elif classinput == "1A":
-    order = Order(0.5, 0.013, 1.05, OA["AD"], 5, 6, 7, 8, 9)
+    order = Order(0.5, 0.013, 1.05, OA["AD"], 5, 0, OA["FD"], OA["FS"], OA["BC"])
     order.AreaDescription(), order.DepthTHU(), order.DepthTVU(), order.FeatureDetection(), order.FeatureSearch(), order.BathymetricCoverage()
 elif classinput == "1B":
-    order = Order(0.5, 0.013, 1.05, OB["AD"], 5, 6, 7, 8, 9)
+    order = Order(0.5, 0.013, 1.05, OB["AD"], 5, 0, OB["FD"], OB["FS"], OB["BC"])
     order.AreaDescription(), order.DepthTHU(), order.DepthTVU(), order.FeatureDetection(), order.FeatureSearch(), order.BathymetricCoverage()
 elif classinput == "2":
-    order = Order(1, 0.023, 1.10, OT["AD"], 20, 6, 7, 8, 9)
+    order = Order(1, 0.023, 1.10, OT["AD"], 20, 0, OT["FD"], OT["FS"], OT["BC"])
     order.AreaDescription(), order.DepthTHU(), order.DepthTVU(), order.FeatureDetection(), order.FeatureSearch(), order.BathymetricCoverage()
 else:
-    print("Wrong input")
+    print("Wrong input. Type either EO, SO, 1A, 1B, 2")
     pass
 
 
---
+++ dictionary_module.py
@@ -0,0 +1,36 @@
+#Dictionary of criteria taken from S-44
+
+EO = {
+      "AD": "Areas where there is strict minimum underkeel clearance and manoeuvrability criteria.",
+      "FD": "Cubic features > 0.5 m",
+      "FS": "200%",
+      "BC": "200%"
+      }
+
+SO = {
+      "AD": "Areas where underkeel clearance is critical.",
+      "FD": "Cubic features > 1 m",
+      "FS": "100%",
+      "BC": "100%"
+      }
+
+OA = {
+      "AD": "Areas where underkeel clearance is considered not to be critical but features of concern to surface shipping may exist.",
+      "FD": "Cubic features > 2 m in depths down to 40 m, 10% of depth if beyond 40 m",
+      "FS": "100%",
+      "BC": "=<100%"
+      }
+
+OB = {
+      "AD": "Areas where underkeel clearance is not considered to be an issue for the type of surface shipping expected to transit the area.",
+      "FD": "Not Specified",
+      "FS": "Recommended but not required",
+      "BC": "5%"
+      }
+
+OT = {
+      "AD": "Areas where a general description of the seafloor is considered adequate.",
+      "FD": "Not Specified",
+      "FS": "Recommended but not required",
+      "BC": "5%"
+      }
